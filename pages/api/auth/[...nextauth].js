c1fefcf1a3e47c648fd0651853260e3b958c7658
Louis Wong <louis.sc@gmail.com>
2021-05-18 15:11:39 -0400

    user model in backend + next-auth setup (credentials)

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,54 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import User from '../../../backend/models/user';
+import dbConnect from '../../../backend/config/dbConnect';
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        dbConnect();
+
+        const { email, password } = credentials;
+
+        // check if email and password were entered
+        if (!email || !password) {
+          throw new Error('Please enter email and password');
+        }
+
+        // find user in database
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user) {
+          throw new Error('Invalid email or password');
+        }
+
+        // check if password is correct
+        const isPasswordMatched = await user.comparePassword(password);
+
+        if (!isPasswordMatched) {
+          throw new Error('Invalid email or password');
+        }
+
+        return Promise.revolve(user);
+      },
+    }),
+  ],
+
+  // JWT callback is called whenever a jwt token is created
+  callbacks: {
+    jwt: async (token, user) => {
+      user && (token.user = user);
+      return Promise.resolve(token);
+    },
+    // token received in session is the token passed from jwt callback
+    session: async (session, token) => {
+      session.user = token.user;
+      return Promise.resolve(session);
+    },
+  },
+});
---
+++ pages/api/auth/register.js
@@ -0,0 +1,14 @@
+import nc from 'next-connect';
+
+import dbConnect from '../../../backend/config/dbConnect';
+import { registerUser } from '../../../backend/controllers/authControllers';
+
+import onError from '../../../backend/middlewares/errors';
+
+const handler = nc({ onError });
+
+dbConnect();
+
+handler.post(registerUser);
+
+export default handler;
